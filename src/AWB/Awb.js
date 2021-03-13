6f82e9266c7f607b2da55544012ed2fb4b3a5527
dmitridrozdov <dm.drozdov@gmail.com>
2021-03-13 15:17:55 +1100

    Update Awb.js

--- src/AWB/Awb.js
+++ src/AWB/Awb.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import { Grid, Typography, Paper } from '@material-ui/core'
+import useStyles from './styles'
+import LineGraph from './LineGraph'
 
 const Awb = () => {
+    const classes = useStyles()
     return (
-        <div>
-            Under Construction
-        </div>
+        <>
+        <Grid container className={classes.root} spacing={2}>
+            <Grid item xs={12} >
+                <Grid container justify="center" spacing={2} className={classes.firstItem}>
+                    <Grid item xs={2}>
+                        <Paper className={classes.paper}> 
+                        <Typography gutterBottom variant='h5' className={classes.menu}>
+                            Results
+                        </Typography>
+                        <Typography gutterBottom variant='h5' className={classes.menu}>
+                            Logs
+                        </Typography>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={10}>
+                        <Paper className={classes.paper}>
+                        <Typography gutterBottom  className={classes.content}>
+                            super a lot of information super a lot of informationsuper a lot of informationsuper a lot of informationsuper a lot of informationsuper a lot of information
+                            super a lot of information
+                            super a lot of informationsuper a lot of informationsuper a lot of informationsuper a lot of informationsuper a lot of information
+                            super a lot of informationsuper a lot of informationsuper a lot of information
+                        </Typography>
+                        <LineGraph />
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+        </>
     )
 }
 
