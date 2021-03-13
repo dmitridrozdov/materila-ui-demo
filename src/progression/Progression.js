3f72d5a686a0261c9050150ae63cb8c06353269b
dmitridrozdov <dm.drozdov@gmail.com>
2021-03-14 10:57:03 +1100

    Update Progression.js

--- src/progression/Progression.js
+++ src/progression/Progression.js
@@ -1,42 +1,14 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 
-const drawerWidth = 240;
+import { AppBar, Avatar, Drawer, CssBaseline, Toolbar, List, Typography, Divider, ListItem, ListItemIcon, ListItemText, Link } from '@material-ui/core'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  drawerContainer: {
-    overflow: 'auto',
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-}));
+import BlurOnIcon from '@material-ui/icons/BlurOn'
+import AppleIcon from '@material-ui/icons/Apple'
+
+import useStyles from './styles'
+import cbaAvatar from '../images/cba.png'
+
+
 
 export default function Progression() {
   const classes = useStyles();
@@ -47,16 +19,17 @@ export default function Progression() {
   // }
 
   const onItemClick = section => () => {
-    setSection(section);
-  };
+    setSection(section)
+  }
 
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
+      <AppBar position='fixed' className={classes.appBar}>
         <Toolbar>
-          <Typography variant="h6" noWrap>
-            Clipped drawer
+          <Avatar alt='' src={cbaAvatar} />
+          <Typography variant='h6' className={classes.appBarText}>
+            <Link href='/' style={{ textDecoration: 'none', color: 'black' }}>CDO Quality Engineering Services </Link> | Progression
           </Typography>
         </Toolbar>
       </AppBar>
@@ -69,41 +42,36 @@ export default function Progression() {
       >
         <Toolbar />
         <div className={classes.drawerContainer}>
-          {/* <List>
-            {['Home', 'Regression'].map((text, index) => (
-              <ListItem button key={text} onClick={onItemClick('Home')}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text}/>
-              </ListItem>
-            ))}
-          </List> */}
-        <List>
-          <ListItem button onClick={onItemClick('Home')}>
-            <ListItemText>Home</ListItemText>
-          </ListItem>
-          <ListItem button onClick={onItemClick('1')}>
-            <ListItemText>Page 2</ListItemText>
-          </ListItem>
-        </List>
-          <Divider />
-          {/* <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List> */}
+          <List className={classes.list}>
+            <Divider />
+            <ListItem button onClick={onItemClick('Dashboard')}>
+              <ListItemIcon> <BlurOnIcon /></ListItemIcon>
+                <ListItemText
+                  disableTypography
+                  primary={<Typography className={classes.listItemName}>Dashboard</Typography>}
+                />
+            </ListItem>
+            <Divider />
+            <ListItem button onClick={onItemClick('1')}>
+              <ListItemIcon> <AppleIcon /></ListItemIcon>
+              <ListItemText
+                  disableTypography
+                  primary={<Typography className={classes.listItemName}>Calculations</Typography>}
+                />
+            </ListItem>
+            <Divider />
+          </List>
+      
         </div>
       </Drawer>
       <main className={classes.content}>
         <Toolbar />
         <Typography paragraph>
-        {section === 'Home' ? 
+        {section === 'Dashboard' ? 
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt' + 
           'ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum'
         : 
-          'Regression'}
+        <Avatar alt='' src={cbaAvatar} />}
           </Typography>
         
       </main>
