96942ed13667d7f3fcde155446f4dfb372694b9e
dmitridrozdov <dm.drozdov@gmail.com>
2021-03-13 15:17:52 +1100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import HomePage from './homePage/HomePage'
 import Awb from './AWB/Awb'
+import Progression from './progression/Progression'
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
         <Switch>
           <Route path="/" exact component={HomePage} />
           <Route path="/awb" exact component={Awb} />
+          <Route path="/progression" exact component={Progression} />
         </Switch>
       </BrowserRouter>
     )
