4117117274534c08e368c514e7ed64fc992672c6
dmitridrozdov <dm.drozdov@gmail.com>
2021-03-31 17:00:20 +1100

    Create SectionCard.js

---
+++ src/AWB/SectionCard.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import Expand from 'react-expand-animated'
+import { Typography, Card, CardContent } from '@material-ui/core'
+import useStyles from './styles'
+import TestCard from './TestCard'
+
+const SectionCard = ({ sectionName, subSections }) => {
+    const classes = useStyles()
+    const [expandState, SetExpandState] = useState(false)
+
+    const toggle = () => {
+        SetExpandState(!expandState)
+    }
+
+    const noPointer = {cursor: 'pointer'};
+
+    return (        
+        <Card className={classes.sectionCard} variant='outlined' >
+            <CardContent>
+                <Typography gutterBottom variant='h5' className={classes.sectionName} onClick={toggle} style={noPointer}>
+                    {sectionName}
+                </Typography>
+                <Expand open={expandState}>
+                    <Typography className={classes.serviceContent}>
+                        {subSections.map(subsection => <TestCard subsection={subsection} /> )}
+                        
+                    </Typography>
+                </Expand>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default SectionCard
